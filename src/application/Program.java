88ae177b1bd5eb58e064f48935fa568e0bc39cb9
caiopy <caiop.sci@hotmail.com>
2023-01-01 19:13:39 -0300

    Seller Entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1,"teste");
-		System.out.println(obj);
+		
+		Seller slr = new Seller(30,"jhon","jhond@gmail.com",new Date(), 20000.0,obj);
+		
+		System.out.println(slr);
 
 	}
 
