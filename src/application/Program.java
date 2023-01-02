3c8f737b230073d0fee836fb019b5d0577817ffe
caiopy <caiop.sci@hotmail.com>
2023-01-02 09:49:05 -0300

    Seller findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,20 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1,"teste");
 		
-		Seller slr = new Seller(30,"jhon","jhond@gmail.com",new Date(), 20000.0,obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(slr);
+		Seller seller = sellerDao.findById(2);
+		
+		System.out.println(seller);
 
 	}
 
